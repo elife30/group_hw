9788da55df5f8cd2de5938827323777f22628db7
elife <mert_elife@hotmail.com>
2021-10-29 15:33:16 -0400

    track01.java

---
+++ track01.java
@@ -0,0 +1,11 @@
+package gitHomeWork;
+
+public class track01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello my world");
+		
+	}
+
+}
